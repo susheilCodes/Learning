3f9ef29033ea23989bd7b969223c4267489642f5
Trevoir Williams <trevoir.williams@gmail.com>
2023-02-17 18:13:11 -0500

    Update ApplicationServiceRegistration.cs

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -10,7 +10,7 @@ namespace HR.LeaveManagement.Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => Assembly.GetExecutingAssembly());
 
             return services;
         }
